76d196e74e103dd07bc3d58da19ff5326faed16d
rupam7615 <rupamgargrg39@gmail.com>
2022-01-15 11:22:52 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-wwmwdg
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-wwmwdg)
---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./style.css";
+import Home from './component/Home';
+import Aboutus from './component/Aboutus';
+import Contact from './component/Contact';
+import { BrowserRouter as Router,Routes,Route } from
+'react-router-dom'
+
+export default function App() {
+  return (
+    <div>
+<Router>
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/contact" element={<Contact />} />
+    <Route path="/about" element={<Aboutus />} />
+    
+
+    </Routes>
+  </Router>
+     
+    </div>
+  );
+}
